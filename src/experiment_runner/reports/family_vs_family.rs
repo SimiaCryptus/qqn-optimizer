eb0d4324fc4ce4764b62e421971d468330b78085
Andrew Charneski <acharneski@gmail.com>
2025-07-30 10:42:32 -0700

    tmp

--- src/experiment_runner/reports/family_vs_family.rs
+++ src/experiment_runner/reports/family_vs_family.rs
@@ -1,7 +1,7 @@
 use crate::benchmarks::evaluation::{is_no_threshold_mode, BenchmarkResults, ProblemSpec};
 use crate::experiment_runner::experiment_runner::get_optimizer_family;
 use crate::experiment_runner::report_generator;
-use crate::experiment_runner::report_generator::FamilyPerformanceData;
+use crate::experiment_runner::report_generator::{FamilyPerformanceData, escape_latex_safe};
 use anyhow::Context;
 use std::collections::HashMap;
 use std::fs;
